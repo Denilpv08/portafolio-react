e01c14580f425257fe799be999757f6cf7d7540f
Denilpv08 <denilpv22gmail.com>
2023-02-02 23:29:06 -0500

    portafolio react

---
+++ src/data/BlogData.js
@@ -0,0 +1,71 @@
+// blog data
+
+export const Blogs = [
+    {
+        id:1,
+        name:"Cree un sitio web con React JS, con styledComponents y react route dom para sus rutas.",
+        tags:["react","react-route-dom","styled-components"],
+        date:"10 Ene, 2023",
+        imgSrc:"https://raw.githubusercontent.com/Denilpv08/portadas/master/nftpage.png",
+        link:"https://denilpv-nftpage.netlify.app/"
+    },
+    {
+        id:2,
+        name:"La creación de un sitio web para, consumiendo una Api rest de películas",
+        tags:["react", "api"],
+        date:"15 Dic, 2022",
+        imgSrc:"https://raw.githubusercontent.com/Denilpv08/portadas/master/web-movies.png",
+        link:"https://denilpv-movie.netlify.app/"
+    },
+    {
+        id:3,
+        name:"Haciendo un mi primer clon de una dashboard echa con React JS",
+        tags:["react","json"],
+        date:"28 Ene, 2023",
+        imgSrc:"https://raw.githubusercontent.com/Denilpv08/portadas/master/dashboard-game.png",
+        link:"https://denilpv-dashboard-game.netlify.app/"
+    },
+    {
+        id:4,
+        name:"Este es uno de mis primeros proyectos con React JS dándole manejo a los hooks.",
+        tags:["react","hooks"],
+        date:"15 Oct, 2022",
+        imgSrc:"https://raw.githubusercontent.com/Denilpv08/portadas/master/task-list.png",
+        link:"https://denilpv-task-list.netlify.app/"
+    },
+    {
+        id:5,
+        name:"Una calculadora dinámica con React JS para reforzar conocimiento de JavaScript.",
+        tags:["react","hooks", "javascript"],
+        date:"20 Sept, 2022",
+        imgSrc:"https://raw.githubusercontent.com/Denilpv08/portadas/master/calculadora.png",
+        link:"https://denilpv-calculadora.netlify.app/"
+    },
+    {
+        id:6,
+        name:"Haciendo el uso de bases de datos de un servidor llamado Firebase, con un login simple.",
+        tags:["react","firebase"],
+        date:"5 Dic, 2022",
+        imgSrc:"https://raw.githubusercontent.com/Denilpv08/portadas/master/login-react.png",
+        link:"https://denilpv-login-react.netlify.app/"
+
+    },
+   
+    {
+        id:7,
+        name:"Un creador de memes con React JS, muy sencillo pero funcional donde puedes guardar los memes que realices.",
+        tags:["react","hooks"],
+        date:"16 Dic, 2022",
+        imgSrc:"https://raw.githubusercontent.com/Denilpv08/portadas/master/app-memes.png",
+        link:"https://denilpv-memes.netlify.app/"
+    },
+    {
+        id:8,
+        name:"Manejador de click con React JS, dándole uso a los useState y dando buena práctica.",
+        tags:["react","hooks", "javascript"],
+        date:"4 Sept, 2022",
+        imgSrc:"https://raw.githubusercontent.com/Denilpv08/portadas/master/Contador-click.png",
+        link:"https://denilpv-contador-click.netlify.app/"
+    },
+  
+] 
---
+++ src/data/WorkData.js
@@ -0,0 +1,63 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Pagina de NFT",
+        description:"Está construido sobre React JS, con styledComponents y react route dom para sus rutas.",
+        tags:["react","react-route-dom","styledComponents"],
+        
+        demo:"https://denilpv-nftpage.netlify.app/",
+        github:"https://github.com/Denilpv08/nftpage"
+    },
+    {
+        id:2,
+        name:"Paginas de peliculas",
+        description:"Un sitio web de listado de películas creado en ReactJS. Con funciones de mostrar a detalle cada película.",
+        tags:["react", "api"],
+        
+        demo:"https://denilpv-movie.netlify.app/",
+        github:"https://github.com/Denilpv08/web-movie"
+        
+    },
+    {
+        id:3,
+        name:"Clon de una Dashboard Game",
+        description:"Un clon de Dashboard Gama usando ReactJS. Consumiendo un formato JSON local.",
+        tags:["react","json"],
+        
+        demo:"https://denilpv-dashboard-game.netlify.app/",
+        github:"https://github.com/Denilpv08/dashboard-ui-game"
+    },
+    {
+        id:4,
+        name:"Lista de tareas",
+        description:"Un generador de lista de tareas creado con ReactJS y componentes con estilo.",
+        tags:["react","hooks"],
+        
+        demo:"https://denilpv-task-list.netlify.app/",
+        github:"https://github.com/Denilpv08/Task-list"
+       
+    },{
+        id:5,
+        name:"Calculadora dinámica",
+        description:"Una calculadora creada con ReactJS, con una experiencia de usuario buena y muy dinámica.",
+        tags:["react","hooks"],
+        
+        demo:"https://denilpv-calculadora.netlify.app/",
+        github:"https://github.com/Denilpv08/calculadora-react"
+        
+    },
+    {
+        id:6,
+        name:"Login con Firabase",
+        description:"Un login en React JS y FireBase con funcionalidades de registrar usuario. Construido a partir de cero.",
+        tags:["react","firebase"],
+        
+        demo:"https://denilpv-login-react.netlify.app/",
+        github:"https://github.com/Denilpv08/login-react"
+        
+    },
+    
+  
+] 
